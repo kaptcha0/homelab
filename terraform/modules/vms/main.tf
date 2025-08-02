6761568dcc783998c2b8af48d8a601bbb8a56a26
J'C Kabunga (kaptcha0) <50426223+kaptcha0@users.noreply.github.com>
2025-08-01 23:10:35 -0400

    added basic configuration for entire infrastructure

--- terraform/modules/vms/main.tf
+++ terraform/modules/vms/main.tf
@@ -2,25 +2,13 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.78.2"
-    }
-    sops = {
-      source  = "carlpett/sops"
-      version = "1.2.0"
+      version = "~>0.81.0"
     }
   }
 }
 
-provider "proxmox" {
-  endpoint = var.pm_api_url
-
-  api_token = local.pm_api_token
-  insecure  = true
-
-  ssh {
-    agent    = true
-    username = var.pm_user
-  }
+module "shared" {
+  source = "../shared/"
 }
 
 locals {
@@ -32,5 +20,4 @@ locals {
     v4 = flatten([for vm in proxmox_virtual_environment_vm.k3s_agent : vm.ipv4_addresses])
     v6 = flatten([for vm in proxmox_virtual_environment_vm.k3s_agent : vm.ipv6_addresses])
   }
-  pm_api_token = "${data.sops_file.pm_api.data["pm_api_token_id"]}=${data.sops_file.pm_api.data["pm_api_token_secret"]}"
 }
