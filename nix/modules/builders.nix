1d4d88ff1c63202d8b566625ccdc6df7e7149f35
J'C Kabunga (kaptcha0) <50426223+kaptcha0@users.noreply.github.com>
2026-01-24 12:03:34 -0500

    feat(nix): corrected labels

--- nix/modules/builders.nix
+++ nix/modules/builders.nix
@@ -1,26 +1,31 @@
 {
   consul =
-    { name, port, domain }:
+    {
+      name,
+      port,
+      domain,
+      checks ? [
+        {
+          http = "http://127.0.0.1:${toString port}";
+          interval = "10s";
+        }
+      ],
+      tags ? [ ],
+      rules ? "",
+    }:
     {
       environment.etc."consul.d/${name}.json".text = builtins.toJSON {
         service = {
-          name = name;
-          port = port;
-
-          checks = [
-            {
-              http = "http://127.0.0.1:${toString port}";
-              interval = "10s";
-            }
-          ];
+          inherit name port checks;
 
           tags = [
             "traefik.enable=true"
-            "traefik.http.routers.vaultwarden.rule=${domain}"
+            "traefik.http.routers.vaultwarden.rule=Host(`${domain}`) ${rules}"
             "traefik.http.routers.vaultwarden.entrypoints=web"
             "traefik.http.routers.vaultwarden.entrypoints=websecure"
             "traefik.http.routers.vaultwarden.tls.certresolver=cloudflare"
-          ];
+          ]
+          ++ tags;
         };
       };
 
