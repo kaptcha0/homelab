583e0df3a6e9794ba70bcfe3b2d95c05342ce647
J'C Kabunga (kaptcha0) <50426223+kaptcha0@users.noreply.github.com>
2026-01-25 09:59:35 -0500

    fix(vaultwarden): corrected 404 error by removing domain arguement

--- nix/hosts/vaultwarden/default.nix
+++ nix/hosts/vaultwarden/default.nix
@@ -5,25 +5,30 @@ let
   builders = import ./../../modules/builders.nix;
 in
 {
-  # sops.defaultSopsFile = ./secrets.yaml;
-  # sops.secrets.env = { };
+  sops.defaultSopsFile = ./secrets.yaml;
+  sops.secrets.env = { };
 
   environment.etc = (
     builders.consul {
       inherit port domain;
       name = "vaultwarden";
+      checks = [
+        {
+          http = "http://127.0.0.1:${toString port}/api/alive";
+          interval = "10s";
+        }
+      ];
     }
   );
 
   services.vaultwarden = {
     enable = true;
-    domain = "https://${domain}";
     config = {
       rocketAddress = "0.0.0.0";
       rocketPort = port;
     };
 
-    # environmentFile = config.sops.secrets.env.path;
+    environmentFile = config.sops.secrets.env.path;
   };
 
   networking.firewall.allowedTCPPorts = [ config.services.vaultwarden.config.rocketPort ];
