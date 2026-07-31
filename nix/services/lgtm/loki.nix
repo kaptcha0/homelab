c3401361be8d1b4dfab6910d24b7d93c163ca8f9
J'C Kabunga (kaptcha0) <50426223+kaptcha0@users.noreply.github.com>
2026-07-31 00:03:15 -0400

    feat: added alloy config for loki

--- nix/services/lgtm/loki.nix
+++ nix/services/lgtm/loki.nix
@@ -1,1 +1,80 @@
-{ ... }: {}
+{ ... }:
+let
+  builders = import ./../../modules/builders.nix;
+  port = 9091;
+in
+{
+  environment.etc = (
+    builders.consul {
+      inherit port;
+      enableTraefik = false;
+      name = "prometheus";
+      checks = [
+        {
+          http = "http://127.0.0.1:${toString port}/-/healthy";
+          interval = "10s";
+        }
+        {
+          http = "http://127.0.0.1:${toString port}/-/ready";
+          interval = "10s";
+        }
+      ];
+    }
+  );
+
+  services.loki = {
+    enable = true;
+    configuration =
+      let
+        path = "/var/lib/loki";
+      in
+      {
+        auth_enabled = false;
+        server.http_listen_port = port;
+
+        common = {
+          replication_factor = 1;
+          ring.kvstore.store = "inmemory";
+
+          path_prefix = path;
+
+          storage.filesystem = {
+            chunks_directory = path + "/chunks";
+            rules_directory = path + "/rules";
+          };
+        };
+
+        schema_config.configs = [
+          {
+            from = "2026-06-30";
+            store = "tsdb";
+            object_store = "filesystem";
+            schema = "v13";
+            index = {
+              prefix = "index_";
+              period = "24h";
+            };
+          }
+        ];
+
+        limits_config = {
+          retention_period = (toString (30 * 24)) + "h";
+          reject_old_samples = true;
+          reject_old_samples_max_age = (toString (7 * 24)) + "h";
+          max_streams_per_user = 0;
+          ingestion_rate_mb = 16;
+          ingestion_burst_size_mb = 32;
+        };
+
+        compactor = {
+          working_directory = path + "/compactor";
+          compaction_interval = "10m";
+          retention_enabled = true;
+          retention_delete_delay = "2h";
+          delete_request_store = "filesystem";
+        };
+      };
+  };
+
+  networking.firewall.allowedTCPPorts = [ port ];
+}
